040c203c1a91b72da06f83599ea1b1a2cd6e829f
EUROPE\hunded <demeter.dobos@sas.com>
2020-11-24 18:31:50 +0100

    3rd party Slider komponens

---
+++ src/components/Slider/Slider.svelte
@@ -0,0 +1,15 @@
+<script>
+  import RangeSlider from 'svelte-range-slider-pips';
+  export let min;
+  export let max;
+  export let values;
+  export let range;
+  export let pushy;
+  export let float = true;
+</script>
+
+<style>
+
+</style>
+
+<RangeSlider min={min} max={max} {range} {pushy} {float} bind:values={values}/>
